45cc59e9f5233725c192898d0fc38669e0ffcd40
AdelinaPatlatii <adelina.patlatii@mail.utoronto.ca>
2025-07-26 15:22:08 +0300

    Added a minimally working fibonacci calculator page

--- api/routes.py
+++ api/routes.py
@@ -1,8 +1,24 @@
-from fastapi import APIRouter, Query
 from api import controllers
+from fastapi import APIRouter, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+@router.get("/fib")
+def home(request: Request):
+    return templates.TemplateResponse("fibonacci.html", {"request": request, "title": "Calculate the n-th Fibonacci number"})
 
-@router.get("/fib", response_model=int, tags=["Fancy mathematics operations"], summary="Calculate the n-th Fibonacci number")
-def fib_endpoint(n: int = Query(..., ge=0, description="Index of the Fibonacci number (must be ≥ 0 !!!!)")) -> int:
-    return controllers.fib(n)
+
+@router.post("/fib", response_class=HTMLResponse)
+async def compute_fib(request: Request, n: int = Form(...)):
+    result = controllers.fib(n)
+    return templates.TemplateResponse(
+        "fibonacci.html",
+        {
+            "request": request,
+            "n": n,
+            "fib_result": result
+        }
+    )
--- main.py
+++ main.py
@@ -5,11 +5,19 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 import os
+import uvicorn
 
 app = FastAPI()
 app.include_router(router)
 
+# static stuff mounted
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# template rendering
 templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "title": "The Fancy Calculator"})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=2025)
